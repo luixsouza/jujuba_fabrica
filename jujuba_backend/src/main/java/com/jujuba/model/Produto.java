5a59824652298f32650bb81bb1ec633099b41b7d
Luis Eduardo <luixst1@gmail.com>
2025-01-27 22:50:45 -0300

    /refact(backend): Refatorando a classe modelo Produto e adicionando lombok

--- jujuba_backend/src/main/java/com/jujuba/model/Produto.java
+++ jujuba_backend/src/main/java/com/jujuba/model/Produto.java
@@ -1,58 +1,64 @@
 package com.jujuba.model;
 
+import jakarta.persistence.Column;
 import jakarta.persistence.Entity;
 import jakarta.persistence.GeneratedValue;
 import jakarta.persistence.GenerationType;
 import jakarta.persistence.Id;
+import jakarta.persistence.Table;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
 
+@Getter @Setter @NoArgsConstructor
 @Entity
+@Table(name = "produto")
 public class Produto {
 
     @Id
     @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
     private Long id;
+    
+    @Column (name = "nome", nullable = false, length = 200)
     private String nome;
+
+    @Column (name = "descricao", nullable = false, length = 300)
     private String descricao;
+
+    @Column (name = "codigo", nullable = false, length = 50)
     private String codigo;
+
+    @Column (name = "preco", nullable = false)
     private Double preco;
 
-    public Long getId() {
-        return id;
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
     }
 
-    public void setId(Long id) {
-        this.id = id;
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Produto other = (Produto) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
     }
 
-    public String getNome() {
-        return nome;
-    }
-
-    public void setNome(String nome) {
-        this.nome = nome;
-    }
-
-    public String getDescricao() {
-        return descricao;
-    }
-
-    public void setDescricao(String descricao) {
-        this.descricao = descricao;
-    }
-
-    public String getCodigo() {
-        return codigo;
-    }
-
-    public void setCodigo(String codigo) {
-        this.codigo = codigo;
-    }
-
-    public Double getPreco() {
-        return preco;
-    }
-
-    public void setPreco(Double preco) {
-        this.preco = preco;
+    @Override
+    public String toString() {
+        return "Produto [id=" + id + "]";
     }
 }
