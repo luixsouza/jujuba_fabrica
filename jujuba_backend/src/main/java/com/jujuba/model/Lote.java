9c232082587c8a315cdce95ed6db0c36285dbf98
Luis Eduardo <luixst1@gmail.com>
2025-02-02 21:54:28 -0300

    /feat(backend): implementando model Lote e relacionando com fornecedora

---
+++ jujuba_backend/src/main/java/com/jujuba/model/Lote.java
@@ -0,0 +1,67 @@
+package com.jujuba.model;
+
+import java.time.LocalDateTime;
+
+import org.springframework.data.annotation.CreatedDate;
+
+import jakarta.persistence.Column;
+import jakarta.persistence.Entity;
+import jakarta.persistence.GeneratedValue;
+import jakarta.persistence.GenerationType;
+import jakarta.persistence.Id;
+import jakarta.persistence.JoinColumn;
+import jakarta.persistence.ManyToOne;
+import jakarta.persistence.Table;
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter @Setter @NoArgsConstructor @AllArgsConstructor
+@Entity
+@Table(name = "lote")
+public class Lote {
+
+    @Id
+    @GeneratedValue(strategy = GenerationType.IDENTITY)
+    @Column(name = "id")
+    private Long id;
+
+    @CreatedDate
+    @Column(name = "data_criacao_lote")
+    private LocalDateTime datacriação;
+
+    @ManyToOne(optional = false)
+    @JoinColumn(name = "fornecedora_id", nullable = false)
+    private Fornecedora fornecedora;
+
+    @Override
+    public int hashCode() {
+        final int prime = 31;
+        int result = 1;
+        result = prime * result + ((id == null) ? 0 : id.hashCode());
+        return result;
+    }
+
+    @Override
+    public boolean equals(Object obj) {
+        if (this == obj)
+            return true;
+        if (obj == null)
+            return false;
+        if (getClass() != obj.getClass())
+            return false;
+        Lote other = (Lote) obj;
+        if (id == null) {
+            if (other.id != null)
+                return false;
+        } else if (!id.equals(other.id))
+            return false;
+        return true;
+    }
+
+    @Override
+    public String toString() {
+        return "Lote [id=" + id + ", datacriação=" + datacriação + ", fornecedora=" + fornecedora + "]";
+    }
+}
