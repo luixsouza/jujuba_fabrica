95fe81a496a8c972984afc821f7e92d2fe7174b7
Luis Eduardo <luixst1@gmail.com>
2025-02-21 12:19:40 -0300

    [refactor] Alteração nos models, removendo redundancia de equal e hashcode

--- jujuba_backend/src/main/java/com/jujuba/model/Lote.java
+++ jujuba_backend/src/main/java/com/jujuba/model/Lote.java
@@ -38,34 +38,4 @@ public class Lote {
     @ManyToOne(optional = false)
     @JoinColumn(name = "produto_id", nullable = false)
     private Produto produto;
-
-    @Override
-    public int hashCode() {
-        final int prime = 31;
-        int result = 1;
-        result = prime * result + ((id == null) ? 0 : id.hashCode());
-        return result;
-    }
-
-    @Override
-    public boolean equals(Object obj) {
-        if (this == obj)
-            return true;
-        if (obj == null)
-            return false;
-        if (getClass() != obj.getClass())
-            return false;
-        Lote other = (Lote) obj;
-        if (id == null) {
-            if (other.id != null)
-                return false;
-        } else if (!id.equals(other.id))
-            return false;
-        return true;
-    }
-
-    @Override
-    public String toString() {
-        return "Lote [id=" + id + ", datacriação=" + datacriação + ", fornecedora=" + fornecedora + "]";
-    }
 }
