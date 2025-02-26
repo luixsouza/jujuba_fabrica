7d2152a24f6956ab3507459786f9c223a4e98860
Luis Eduardo <luixst1@gmail.com>
2025-02-26 17:37:07 -0300

    /remove(backend): Removendo redundancias entre Produtos e Lotes

--- jujuba_backend/src/main/java/com/jujuba/service/ProdutoService.java
+++ jujuba_backend/src/main/java/com/jujuba/service/ProdutoService.java
@@ -1,8 +1,6 @@
 package com.jujuba.service;
 
-import com.jujuba.model.Lote;
 import com.jujuba.model.Produto;
-import com.jujuba.repository.LoteRepository;
 import com.jujuba.repository.ProdutoRepository;
 
 import lombok.RequiredArgsConstructor;
@@ -10,41 +8,19 @@ import lombok.RequiredArgsConstructor;
 import org.springframework.stereotype.Service;
 
 import java.util.List;
-import java.util.Optional;
 
 @Service
 @RequiredArgsConstructor
 public class ProdutoService {
 
-    private ProdutoRepository produtoRepository;
-    private LoteRepository loteRepository;
+    private final ProdutoRepository produtoRepository;
 
     public List<Produto> listarTodos() {
         return produtoRepository.findAll();
     }
 
-    public Produto salvar(Produto produto) {
-        Optional<Lote> lote = loteRepository.findById(produto.getLote().getId());
-        if (lote.isEmpty()) {
-            throw new IllegalArgumentException("Lote não encontrado.");
-        }
-        return produtoRepository.save(produto);
-    }
-
     public Produto buscarPorId(Long id) {
         return produtoRepository.findById(id).orElse(null);
-    }
-
-    public Produto atualizar(Long id, Produto produtoAtualizado) {
-        Produto produtoExistente = buscarPorId(id);
-        if (produtoExistente != null) {
-            //produtoExistente.setNome(produtoAtualizado.getNome());
-            produtoExistente.setDescricao(produtoAtualizado.getDescricao());
-            //produtoExistente.setCodigo(produtoAtualizado.getCodigo());
-            produtoExistente.setPreco(produtoAtualizado.getPreco());
-            return produtoRepository.save(produtoExistente);
-        }
-        return null;
     }
 
     public void excluir(Long id) {
