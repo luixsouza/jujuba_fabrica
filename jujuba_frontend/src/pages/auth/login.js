b1490a3b27b88c0067b52ad8c1d075679293aac5
Fernanda Ribeiro <nandarsgomes3@gmail.com>
2025-06-17 20:04:40 -0300

    alteracao login

--- jujuba_frontend/src/pages/auth/login.js
+++ jujuba_frontend/src/pages/auth/login.js
@@ -3,7 +3,17 @@
 import { useState } from "react"
 import { useRouter } from "next/navigation"
 import Link from "next/link"
-import { Box, Typography, Button, TextField, Container, Paper, InputAdornment, IconButton } from "@mui/material"
+import Image from "next/image"
+import {
+  Box,
+  Typography,
+  Button,
+  TextField,
+  Container,
+  Paper,
+  InputAdornment,
+  IconButton,
+} from "@mui/material"
 import VisibilityIcon from "@mui/icons-material/Visibility"
 import VisibilityOffIcon from "@mui/icons-material/VisibilityOff"
 
@@ -63,48 +73,51 @@ export default function Login() {
               padding: 4,
             }}
           >
-            {/* Clouds */}
-            <Box
-              sx={{
+            {/* Nuvens com imagem */}
+            <Image
+              src="/Imagens/Nuvem.png"
+              alt="Nuvem"
+              width={110}
+              height={70}
+              style={{
                 position: "absolute",
                 top: "15%",
                 left: "15%",
-                width: "60px",
-                height: "30px",
-                backgroundColor: "white",
-                borderRadius: "30px",
+                objectFit: "contain",
               }}
             />
-            <Box
-              sx={{
+            <Image
+              src="/Imagens/Nuvem.png"
+              alt="Nuvem"
+              width={110}
+              height={70}
+              style={{
                 position: "absolute",
                 bottom: "20%",
                 right: "20%",
-                width: "80px",
-                height: "40px",
-                backgroundColor: "white",
-                borderRadius: "40px",
+                objectFit: "contain",
               }}
             />
-            <Box
-              sx={{
+            <Image
+              src="/Imagens/Nuvem.png"
+              alt="Nuvem"
+              width={70}
+              height={50}
+              style={{
                 position: "absolute",
                 top: "70%",
                 left: "25%",
-                width: "50px",
-                height: "25px",
-                backgroundColor: "white",
-                borderRadius: "25px",
+                objectFit: "contain",
               }}
             />
 
-            {/* Logo */}
+            {/* Logo com imagem */}
             <Box
               sx={{
                 width: "180px",
                 height: "180px",
                 borderRadius: "50%",
-                backgroundColor: "#FFD700",
+                backgroundColor: "#FFF9ac",
                 display: "flex",
                 justifyContent: "center",
                 alignItems: "center",
@@ -122,22 +135,20 @@ export default function Login() {
                   justifyContent: "center",
                   alignItems: "center",
                   border: "4px solid white",
+                  overflow: "hidden",
                 }}
               >
-                <Typography
-                  sx={{
-                    fontFamily: "'Comic Sans MS', cursive",
-                    fontSize: "24px",
-                    color: "#FF69B4",
-                    fontWeight: "bold",
-                  }}
-                >
-                  jujuba
-                </Typography>
+                <Image
+                  src="/Imagens/image-Photoroom.png"
+                  alt="Logo"
+                  width={345}
+                  height={242}
+                  style={{ borderRadius: "50%" }}
+                />
               </Box>
             </Box>
 
-            {/* Circular rings around the logo */}
+            {/* Anéis ao redor da logo */}
             {[1, 2, 3, 4].map((i) => (
               <Box
                 key={i}
